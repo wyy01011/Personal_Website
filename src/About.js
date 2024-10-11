c086912bef47ce2498d006ade3cfc0b79090c2c8
Valarie <valarie.email@gmail.com>
2024-10-11 11:44:01 -0400

    Started first paragraph for About Me page

--- src/About.js
+++ src/About.js
@@ -1,10 +1,25 @@
 function About() {
   return (
-    <section id="About" className="sectionSkilledAt">
-    <h2>AAA</h2>
-</section>
 
-      
+    <div>
+
+    <header>
+    <h1>About Me </h1>
+    </header>
+   
+
+    <div className = "p1">
+
+      <div class = "pic1">
+        <img src="/images/adobe.png" alt="Picture of me"></img></div>
+
+      <div class = "description1">
+        <p>I am from Hong Kong...</p>
+      </div>
+
+      </div>
+
+    </div>
     
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App'; // Ensure the path is correct
 import './App.css';
 import Homepage from './Homepage'; 
 import './Homepage.css';
+import './About.css';
 import AppRouter from './App';
 
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
